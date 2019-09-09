777f4242337eb1d3d76e2568484b971df0ff1b9c
Achim Munene <achim_munene@hotmail.com>
2019-09-09 22:17:08 +0200

    Added more stuff

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,13 +1,14 @@
-from rest_framework.views import APIView
+from rest_framework import status, viewsets
+from rest_framework.authentication import TokenAuthentication
 from rest_framework.response import Response
-from rest_framework import status 
-from rest_framework import viewsets
+from rest_framework.views import APIView
 
-from profiles_api import serializers
-from profiles_api import models
+from profiles_api import models, permissions, serializers
 
 
 class UserProfileViewSet(viewsets.ModelViewSet):
     """Handle creating and updating profiles"""
     serializer_class = serializers.UserProfileSetializer
     queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
