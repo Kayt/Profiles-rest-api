24302e2f556d3bbbb25af94a6d81c54b28fc24b8
Achim Munene <achim_munene@hotmail.com>
2019-09-19 22:49:08 +0200

    added login

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -8,5 +8,6 @@ router = DefaultRouter()
 router.register('profile', views.UserProfileViewSet)
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('login/', views.UserLoginAPIView.as_view(), name='login'),
+    path('', include(router.urls)),
 ]
